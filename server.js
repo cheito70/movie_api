527faa05ffef9196eb61e111496e746e04ceb5bd
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-08-03 16:59:30 -0500

    Updated code to server.js
    Integrated fs module, url module, and HTTP module with appropriate code

--- server.js
+++ server.js
@@ -4,8 +4,32 @@ const url = require('url');
 
 
 http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n');
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filePath = '';
+      if (q.pathname.includes('documentation')) {
+        filepath = (_dirname + '/documentation.html');
+      } else {
+        filePath = 'index.html';
+      }
+
+      fs.readFile(filePath,(err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+
+      });
+      fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Added to log.');
+        }
+      });    
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');

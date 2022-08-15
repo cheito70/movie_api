31a6e96286d312f01cf7a12ee8de0c402f4a4da1
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-08-15 10:08:31 -0500

    Started adding code for the task
    Did the require for morgan, express, set up get for the /movie.
    Got a bit stuck there.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const express = require('express');
+  morgan = require('morgan');
+
 const app = express();
+
+//Create a write stream in append mode. Also a 'log.txt' file is created in root.
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a'})
+
+app.use(morgan('combined')); //sets up logger midware function to terminal
+
+app.use(express.static('public')); //Automatically routes all requests for static files  to corresponding folder on server
+
+
+//GET returning JSON object containing data about top ten movies
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    })
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to My Fave Flix API!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname});
+});

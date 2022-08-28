2c84ead113eb1f8358748640b8be841e493f3f5f
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-08-28 10:26:58 -0500

    Created a Movies.js file to test my logic

---
+++ Movies.js
@@ -0,0 +1,8 @@
+let Movies = [
+  {
+    title: 'The Shawshank Redemption',
+    description: 'It tells the story of banker, who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence.',
+    genre: 'Drama'
+  }
+
+]

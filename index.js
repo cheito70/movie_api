1c8681d71c0b26cbb7c642d192f8a16cb1ec48ec
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-09-03 16:42:33 -0500

    Updated to "" as opposed to ''

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express'),
-  morgan = require('morgan'),
-  bodyParser = require('body-parser'),
-  fs = require('fs'),
-  path = require('path');
+const express = require("express"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  fs = require("fs"),
+  path = require("path");
 
 const app = express();
-const uuid = require('uuid');
-const methodOverride = require('method-override');
+const uuid = require("uuid");
+const methodOverride = require("method-override");
 
 
 //Create a write stream in append mode. Also a 'log.txt' file is created in root.
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a'})
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { flags: "a"})
 
-app.use(morgan('combined', {stream: accessLogStream})); //sets up logger midware function to terminal
+app.use(morgan("combined", {stream: accessLogStream})); //sets up logger midware function to terminal
 
-app.use("/documentation.html", express.static('public')); //Automatically routes all requests for static files  to corresponding folder on server
+app.use("/documentation.html", express.static("public")); //Automatically routes all requests for static files  to corresponding folder on server
 
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 //GET returning JSON object containing data about top ten movies
-app.get('/movies', (req, res) => {
+app.get("/movies", (req, res) => {
   res.json(movies);
 });
 
@@ -49,27 +49,32 @@ app.get("/movies/genre/:genreName", (req, res) => {
   }
 });
 
+//Get request for director names
+
+
+
+
 //User objects here
 let users = [
   {
     id: 1,
-    name: 'Constance',
-    favoriteMovies: ['When Harry Met Sally']
+    name: "Constance",
+    favoriteMovies: ["When Harry Met Sally"]
   },
   {
     id: 2,
-    name: 'Brian',
+    name: "Brian",
     favoriteMovies: []
   },
   {
     id: 3,
-    name: 'Jennifer',
+    name: "Jennifer",
     favoriteMovies: []
   },
   {
     id: 4,
-    name: 'Arjuna',
-    favoriteMovies: ['Contact']
+    name: "Arjuna",
+    favoriteMovies: ["Contact"]
   }
 ];
 
@@ -77,52 +82,52 @@ let users = [
 
 let movies = [
   {
-    Title : 'Shawshank Redemption',
-    Genre : { Name: 'Drama' },
-    Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    Director: { Name: 'Frank Darabont' },
-    ImageURL: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg'
+    Title : "Shawshank Redemption",
+    Genre : { Name: "Drama" },
+    Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    Director: { Name: "Frank Darabont" },
+    ImageURL: "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg"
   },
   {
-    Title : 'Close Encounters of the Third Kind',
-    Genre : { Name: 'Sci-Fi' },
-    Description: 'Roy Neary, an Indiana electric lineman, finds his quiet and ordinary daily life turned upside down after a close encounter with a UFO, spurring him to an obsessed cross-country quest for answers as a momentous event approaches.',
-    Director: { Name: 'Steven Spielberg' },
-    ImageURL: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5957/5957508_sa.jpg'
+    Title : "Close Encounters of the Third Kind",
+    Genre : { Name: "Sci-Fi" },
+    Description: "Roy Neary, an Indiana electric lineman, finds his quiet and ordinary daily life turned upside down after a close encounter with a UFO, spurring him to an obsessed cross-country quest for answers as a momentous event approaches.",
+    Director: { Name: "Steven Spielberg" },
+    ImageURL: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5957/5957508_sa.jpg"
   },
   {
-    Title : 'Contact',
-    Genre : { Name: 'Sci-Fi' },
-    Description: 'Dr. Ellie Arroway, after years of searching, finds conclusive radio proof of extraterrestrial intelligence, sending plans for a mysterious machine.',
-    Director: { Name: 'Robert Zemeckis' },
+    Title : "Contact",
+    Genre : { Name: "Sci-Fi" },
+    Description: "Dr. Ellie Arroway, after years of searching, finds conclusive radio proof of extraterrestrial intelligence, sending plans for a mysterious machine.",
+    Director: { Name: "Robert Zemeckis" },
     ImageURL: "https://upload.wikimedia.org/wikipedia/en/7/75/Contact_ver2.jpg"
   },
   {
-    Title : 'Thor',
-    Genre : { Name: 'Sci-Fi' },
-    Description: 'The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.',
-    Director: { Name: 'Kenneth Branagh' },
-    ImageURL: 'https://upload.wikimedia.org/wikipedia/en/9/95/Thor_%28film%29_poster.jpg'
+    Title : "Thor",
+    Genre : { Name: "Sci-Fi" },
+    Description: "The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
+    Director: { Name: "Kenneth Branagh" },
+    ImageURL: "https://upload.wikimedia.org/wikipedia/en/9/95/Thor_%28film%29_poster.jpg"
   },
   {
-    Title : 'A River Runs Through It',
-    Genre : { Name: 'Drama' },
-    Description: 'Two sons of a stern minister - one reserved, one rebellious - grow up in rural 1920s Montana while devoted to fly fishing.',
-    Director: { Name: 'Robert Redford' },
-    ImageURL: 'https://upload.wikimedia.org/wikipedia/en/c/ce/A_river_runs_through_it_cover.jpg'
+    Title : "A River Runs Through It",
+    Genre : { Name: "Drama" },
+    Description: "Two sons of a stern minister - one reserved, one rebellious - grow up in rural 1920s Montana while devoted to fly fishing.",
+    Director: { Name: "Robert Redford" },
+    ImageURL: "https://upload.wikimedia.org/wikipedia/en/c/ce/A_river_runs_through_it_cover.jpg"
   },
   {
-    Title : 'When Harry Met Sally',
-    Genre : { Name: 'Romantic Comedy' },
-    Description: 'Harry and Sally have known each other for years, and are very good friends, but they fear sex would ruin the friendship',
-    Director: { Name: 'Rob Reiner' },
+    Title : "When Harry Met Sally",
+    Genre : { Name: "Romantic Comedy" },
+    Description: "Harry and Sally have known each other for years, and are very good friends, but they fear sex would ruin the friendship",
+    Director: { Name: "Rob Reiner" },
     ImageURL: "https://upload.wikimedia.org/wikipedia/en/1/1c/WhenHarryMetSallyPoster.jpg"
   },
   {
-    Title : 'Book of Love',
-    Genre : { Name: 'Romantic Comedy' },
-    Description: 'Two writers thrown together on a book tour in Mexico.',
-    Director: { Name: 'Analeine Cal y Mayor' },
+    Title : "Book of Love",
+    Genre : { Name: "Romantic Comedy" },
+    Description: "Two writers thrown together on a book tour in Mexico.",
+    Director: { Name: "Analeine Cal y Mayor" },
     ImageURL: "https://upload.wikimedia.org/wikipedia/en/e/ea/Bookoflove_cover.jpg"
   }
 ];
@@ -130,21 +135,21 @@ let movies = [
 
 
 //Default textual response
-app.get('/', (req, res) => {
-  res.send('Welcome to My Fave Flix API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to My Fave Flix API!");
 });
 
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname});
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname});
 });
 
 //Error handling code
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke! Sorry.....')
+  res.status(500).send("Something broke! Sorry.....");
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
-  console.log('Listening on Port' + port);
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port" + port);
 });

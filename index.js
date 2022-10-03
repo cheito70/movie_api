4961fb500a4db75abad3751a93fafc1c37d86371
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-03 12:51:58 -0500

    Edited post users code in the password area

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ app.get("/movies/directors/:Name", (req, res) => {
 
 //Post method for users creating the "/users" endpoint and creating new users
 app.post("/users", (req, res) => {
+  let hashedPassword = Users.hashedPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
   .then((user) => {
     if (user) {
@@ -84,7 +85,7 @@ app.post("/users", (req, res) => {
       Users
       .create({
         Username: req.body.Username,
-        Password: req.body.Password,
+        Password: hashedPassword,
         Email: req.body.Email,
         Birthday: req.body.Birthday
       })

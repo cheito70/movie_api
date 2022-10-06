ea8603ed873353c31acc73fec43162b001c69148
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-06 10:35:34 -0500

    Trying to fix validation error on create new user

--- models.js
+++ models.js
@@ -17,9 +17,9 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+  Username: String,
+  Password: String,
+  Email: String,
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });

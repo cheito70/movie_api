cad3522bb92f874392468f514acdca7fd6a668e5
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-12 14:02:40 -0500

    Reverted to original code in passport.js

--- passport.js
+++ passport.js
@@ -22,10 +22,7 @@ passport.use(new LocalStrategy({
       console.log('incorrect username');
       return callback(null, false, { message: 'Incorrect username or password.'});
     }
-    if(!user.validatePassword(password)) {
-      console.log('incorrect password');
-      return callback(null, false, { message: 'incorrect password.' });
-    }
+    
     console.log('finished');
     return callback(null, user);
   });

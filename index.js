e8ca727d2394245af87c782fc36bf6d997b2edc5
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-12 13:36:06 -0500

    Reinstated methodoverride dependency

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require("express"),
 
 const app = express();
 const uuid = require("uuid");
-//const methodOverride = require("method-override");
+const methodOverride = require("method-override");
 
 
 //Create a write stream in append mode. Also a 'log.txt' file is created in root.
@@ -20,7 +20,7 @@ app.use(morgan("combined", {stream: accessLogStream})); //sets up logger midware
 app.use("/documentation.html", express.static("public")); //Automatically routes all requests for static files  to corresponding folder on server
 
 app.use(bodyParser.json());
-//app.use(methodOverride());
+app.use(methodOverride());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 //Mongoose code to access mongodb

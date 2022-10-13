e326ec72df7f5f5e3c655483439af364a4571b25
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-13 12:17:54 -0500

    Updated cors code

--- index.js
+++ index.js
@@ -35,6 +35,22 @@ const Users = Models.User;
 mongoose.connect('mongodb://localhost:27017/myFaveFlixDB',
 { useNewUrlParser: true, useUnifiedTopology: true });
 
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      //If a specific origin isn't found on list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin:
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app); //Connects to auth.js and ensures Express is available in auth.js
 
 //Passport

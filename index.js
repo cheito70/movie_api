63029e1f0778d9e56b4e58b740951edc25498042
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-14 14:03:21 -0500

    Changed port

--- index.js
+++ index.js
@@ -292,8 +292,8 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke! Sorry.....");
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
   console.log("Listening on Port" + port);
 });
 

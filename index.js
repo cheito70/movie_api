9c7b25cb2e8b851fac827548219536dd7b940f9c
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-10-21 00:28:20 -0500

    Connect code for atlas included

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ const Users = Models.User;
 
 //Code that allows to connect to mongodb and myFaveFlixDb
 //to perform CRUD operations
-mongoose.connect('mongodb://localhost:27017/myFaveFlixDB',
+/*mongoose.connect('mongodb://localhost:27017/myFaveFlixDB',
+{ useNewUrlParser: true, useUnifiedTopology: true });*/
+
+mongoose.connect('mongodb+srv://myfavoflixo3131:UWS0Gf8T9K1ElDav@careerfoundry1.ha1wdrw.mongodb.net/myFaveFlixDB?retryWrites=true&w=majority',
 { useNewUrlParser: true, useUnifiedTopology: true });
 
 //CORS cross-origin resource sharing, limiting access from domains to API

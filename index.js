a84dab5b0f8e4f4c24b1f0b41c0815b6a8e5e40a
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-11-08 11:30:48 -0600

    Used code from lesson on /movie endpoint

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get("/documentation", (req, res) => {
 });
 
 //returns JSON object containing all movies
-app.get("/movies", (req, res) => {
+/*app.get("/movies", (req, res) => {
   Movies.find()
   .then((movies) => {
     res.json(movies);
@@ -81,6 +81,17 @@ app.get("/movies", (req, res) => {
     console.error(err);
     res.status(400).send("Error: " + err);
   })
+});*/
+
+app.get("/movies", function (req, res) {
+  Movies.find()
+    .then(function (movies) {
+      res.status(201).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
 //Returns JSON object of a movie by title

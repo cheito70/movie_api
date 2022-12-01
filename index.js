27e8f33092c02ab9053bb5929e2bcc54f05ec4d1
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-12-01 14:54:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get("/documentation", (req, res) => {
   })
 });*/
 
-app.get("/movies", function (req, res) {
+app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies);
